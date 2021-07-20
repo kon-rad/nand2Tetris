60771e256ca6341b39209d504c5fc22f2c2d3fee
konradgnat <konradmgnat@gmail.com>
2021-07-19 21:19:38 -0500

    project 4 v2, notes

---
+++ notes.md
@@ -0,0 +1,45 @@
+
+
+
+# Nand2Tetris Part 2
+
+# WEEK 1
+
+### 
+
+
+
+
+
+# 7/10/2021
+ 
+#vm
+29
+
+ 
+#local
+0
+15
+true
+
+push constant 0  
+pop local 0
+push constant 15 
+pop local 1
+push local 1 
+push argument 1
+gt
+pop local 2
+push local 0 	
+push argument 0
+add
+pop local 0
+push local 1 	
+push local 1
+push constant 1
+sub
+add
+pop local 1
+
+
+
--- projects/04_v2/fill/Fill.asm
+++ projects/04_v2/fill/Fill.asm
@@ -12,3 +12,98 @@
 // the screen should remain fully clear as long as no key is pressed.
 
 // Put your code here.
+
+// there are 512 columns - that is 32 columns of 16 bits each (32 * 16 = 512)
+// there are 255 rows
+// keyboard is at 24576
+
+// first screen address is 16384
+// 256 * 32 = 8192
+// 16383 + 8192 = 24575
+// there are 8192 16 bits in the screen
+// last screen address is 24575
+
+@32
+D=A
+@col
+M=D
+
+
+@256
+D=A
+@row
+M=D
+
+(START)
+@j
+M=0
+
+@16384
+D=A
+@screenpt
+M=D
+
+(LOOP_COL)
+
+// set (or reset) i to zero after every row is filled up
+@i
+M=0
+(LOOP_ROW)
+
+// set current bit to black
+// 16384 is the first bit
+
+
+// @24576
+@KBD
+D=M
+@KEY_PRESSED
+D;JNE
+
+// set screen pointer to 0 to make it white if no key is pressed
+@screenpt
+A=M
+M=0
+
+@KEY_NOT_PRESSED
+0;JMP
+
+(KEY_PRESSED)
+// set screen pointer to -1 to make it all black
+@screenpt
+A=M
+M=-1
+
+(KEY_NOT_PRESSED)
+// increment the screen pointer by 1
+@screenpt
+M=M+1
+
+// increment i by 1
+@i
+M=M+1
+D=M
+
+// if i is less than 32 go to top of loop
+@col
+D=D-M
+@LOOP_ROW
+D;JLT
+
+// otherwise it is the end of the row - increment j
+@j
+M=M+1
+
+// if j is greater than 255 then exit loop, otherwise restart the row
+@j
+D=M
+@row
+D=D-M
+@LOOP_COL
+D;JLT
+
+// restart the paint loop
+@START
+0;JMP
+
+
