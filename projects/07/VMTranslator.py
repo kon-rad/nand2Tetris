25cf03f2777fd0d63c0f72424a37b188b4e57266
konradgnat <konradmgnat@gmail.com>
2021-07-21 20:35:58 -0500

    vm translator

--- projects/07/VMTranslator.py
+++ projects/07/VMTranslator.py
@@ -1,20 +1,31 @@
 import sys
 import os
+import re
 from os import listdir
 from os.path import isfile, join
 
 class Parser:
   filePath = ''
   commands = []
-  position = 0
+  comIndex = 0
+  C_ARITHMETIC = 'C_ARITHMETIC'
+  C_PUSH = 'C_PUSH'
+  C_POP = 'C_POP'
+  C_LABEL = 'C_LABEL'
+  C_GOTO = 'C_GOTO'
+  C_IF = 'C_IF'
+  C_FUNCTION = 'C_FUNCTION'
+  C_RETURN = 'C_RETURN'
+  C_CALL = 'C_CALL'
+  ARITHMETIC_COMMANDS = ['add', 'sub', 'neg']
 
   def __init__(self, filePath):
     self.filePath = filePath
-    self.parse_file()
+    self.parseFile()
 
   # removes all comments and white space
   # adds lines to commands list 
-  def parse_file(self):
+  def parseFile(self):
     file = open(self.filePath, 'r')
     content = file.readlines()
     for line in content:
@@ -34,10 +45,23 @@ class Parser:
       self.commands.append(line.replace('\n', ''))
       print('all commands:', self.commands)
 
-  def write_lines(self ,lines, name):
+  def hasMoreCommands(self):
+    return self.comIndex < len(self.commands) - 1
+
+  def commandType(self):
+    line = self.commands[self.comIndex]
+    if re.match(r'^push ', line):
+      return self.C_PUSH
+    if re.match(r'^pop ', line):
+      return self.C_POP
+    if line[0, 4] in self.ARITHMETIC_COMMANDS:
+      return self.C_ARITHMETIC
+
+  def writeLines(self ,lines, name):
     file = open(name, 'w')
     file.writelines(lines)
     file.close()
+
 
 
 def main():
