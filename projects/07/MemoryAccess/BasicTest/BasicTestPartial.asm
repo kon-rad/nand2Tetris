c0caaf921dc063b1de8065d76a5f21e5ba33269b
konradgnat <konradmgnat@gmail.com>
2021-08-27 09:00:40 -0500

    proj 07 progress

--- projects/07/MemoryAccess/BasicTest/BasicTestPartial.asm
+++ projects/07/MemoryAccess/BasicTest/BasicTestPartial.asm
@@ -6,6 +6,18 @@ M=D
 D=A
 @1
 M=D
+@2015
+D=A
+@2
+M=D
+@3015
+D=A
+@3
+M=D
+@4015
+D=A
+@4
+M=D
 // init SP stack pointer variable to value that is in RAM[0]
 @0
 D=M
@@ -15,6 +27,21 @@ M=D
 @1
 D=M
 @LCL
+M=D
+// init ARG stack pointer variable to value that is in RAM[2]
+@2
+D=M
+@ARG
+M=D
+// init THIS stack pointer variable to value that is in RAM[3]
+@3
+D=M
+@THIS
+M=D
+// init THAT stack pointer variable to value that is in RAM[4]
+@4
+D=M
+@THAT
 M=D
 // push constant 10
 @10
@@ -76,6 +103,121 @@ M=D
 @SP
 A=M
 M=D
+// increment SP
+@SP
+M=M+1
+// pop argument 2
+// decrement SP
+@SP
+M=M-1
+@LCL
+D=M
+@2
+D=D+A
+@temp
+M=D
+@SP
+A=M
+D=M
+@temp
+A=M
+M=D
+// decrement SP and remove
+@SP
+A=M
+M=0
+// decrement SP
+@SP
+M=M-1
+// increment SP
+@SP
+M=M+1
+// push constant 11
+@11
+D=A
+// assign D to SP
+@SP
+A=M
+M=D
+// increment SP
+@SP
+M=M+1
+// push argument 2
+@ARG
+D=M
+@2
+D=D+A
+A=D
+D=M
+@SP
+A=M
+M=D
+// assign D to SP
+@SP
+A=M
+M=D
+// increment SP
+@SP
+M=M+1
+// push constant 12
+@12
+D=A
+// assign D to SP
+@SP
+A=M
+M=D
+// increment SP
+@SP
+M=M+1
+// pop this 2
+// decrement SP
+@SP
+M=M-1
+@LCL
+D=M
+@2
+D=D+A
+@temp
+M=D
+@SP
+A=M
+D=M
+@temp
+A=M
+M=D
+// decrement SP and remove
+@SP
+A=M
+M=0
+// decrement SP
+@SP
+M=M-1
+// increment SP
+@SP
+M=M+1
+// pop that 2
+// decrement SP
+@SP
+M=M-1
+@LCL
+D=M
+@2
+D=D+A
+@temp
+M=D
+@SP
+A=M
+D=M
+@temp
+A=M
+M=D
+// decrement SP and remove
+@SP
+A=M
+M=0
+// decrement SP
+@SP
+M=M-1
 // increment SP
 @SP
 M=M+1
--- projects/07/VMTranslator.py
+++ projects/07/VMTranslator.py
@@ -45,6 +45,9 @@ class CodeWriter:
       self.lines.extend(['@2015', 'D=A', '@2', 'M=D', '@3015', 'D=A', '@3', 'M=D', '@4015', 'D=A', '@4', 'M=D']) # ARG, THIS, THAT
     self.lines.extend(['// init SP stack pointer variable to value that is in RAM[0]', '@0', 'D=M', '@SP', 'M=D'])
     self.lines.extend(['// init LCL stack pointer variable to value that is in RAM[1]', '@1', 'D=M', '@LCL', 'M=D'])
+    self.lines.extend(['// init ARG stack pointer variable to value that is in RAM[2]', '@2', 'D=M', '@ARG', 'M=D'])
+    self.lines.extend(['// init THIS stack pointer variable to value that is in RAM[3]', '@3', 'D=M', '@THIS', 'M=D'])
+    self.lines.extend(['// init THAT stack pointer variable to value that is in RAM[4]', '@4', 'D=M', '@THAT', 'M=D'])
 
   def addNewLine(self, line):
     self.lines.append(line)
